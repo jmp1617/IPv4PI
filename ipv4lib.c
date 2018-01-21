3e3fcba11bd6f21e2d225eda65bbae642601823d
Jacob Potter <jmp1617@g.rit.edu>
2018-01-21 18:11:32 +0000

    ipv4 header load complete

--- ipv4lib.c
+++ ipv4lib.c
@@ -45,3 +45,41 @@ Ethernet_Header create_eII_header(){
 //-----------------------------------------------------
 // Load into Memory
 //-----------------------------------------------------
+
+int load_ip_header_f(Packet_Meta pm, IPv4_Header ih){
+    //read in first byte containing both version and IHL
+    uint8_t temp;
+    uint16_t temp16;
+    fread(&temp, 1, 1, pm->packet);
+    ih->version = 0xF & temp; 
+    temp <<= 4;
+    ih->ihl = 0xF & temp;
+    //read in byte containing dscp and ecn
+    fread(&temp, 1, 1, pm->packet);
+    ih->dscp = 0x3F & temp;
+    temp <<= 6;
+    ih->ecn = 0x3 & temp;
+    //read in total length
+    fread(&(ih->total_length), 2, 1, pm->packet);
+    //identification
+    fread(&(ih->identification), 2, 1, pm->packet);
+    //flags and fragment offset
+    fread(&temp16, 2, 1, pm->packet);
+    ih->flags = 0x7 & temp16;
+    temp16 <<= 3;
+    ih->fragment_offset = 0x1FFF & temp16;
+    //ttl byte
+    fread(&(ih->ttl), 1, 1, pm->packet);
+    //protocol
+    fread(&(ih->protocol), 1, 1, pm->packet);
+    //checksum
+    fread(&(ih->header_checksum), 2, 1, pm->packet);
+    //source ip
+    fread(&(ih->source_ip), 4, 1, pm->packet);
+    //dest ip
+    fread(&(ih->destination_ip), 4, 1, pm->packet);
+}
+
+int load_eII_header_f(Packet_Meta pm, Ethernet_Header eh){
+
+}
